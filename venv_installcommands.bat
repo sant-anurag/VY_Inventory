60240ad6f050632c50a4f978366c4d96ceb2b62c
anurag <sant.vihangam@gmail.com>
2021-06-03 11:41:42 +0530

    install commands for packages

---
+++ venv_installcommands.bat
@@ -0,0 +1,11 @@
+pip install opencv-python
+pip install openpyxl
+pip install win32com
+pip install pywin32
+pip install pandas
+pip install num2words
+pip install pyglet
+pip install pyautogui
+pip install tkcalendar
+pip install pillow
+echo "All packages installed"
